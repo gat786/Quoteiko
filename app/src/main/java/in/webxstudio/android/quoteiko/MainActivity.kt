923054347bfbe8bd0209508974cd8d7bb804f8e8
Ganesh Tiwari <gat786@outlook.com>
2020-06-07 15:45:06 +0530

    added icons for fontstyle and made design for change fontstyle ui

--- app/src/main/java/in/webxstudio/android/quoteiko/MainActivity.kt
+++ app/src/main/java/in/webxstudio/android/quoteiko/MainActivity.kt
@@ -2,10 +2,25 @@ package `in`.webxstudio.android.quoteiko
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun ChangeQuote(view: View) {
+        Log.d(TAG,"Change Quote Clicked")
+    }
+    fun ChangeFontStyle(view: View) {
+        Log.d(TAG,"Change FontStyle Clicked")
+    }
+    fun ChangeImage(view: View) {
+        Log.d(TAG,"Change Image Clicked")
     }
 }
